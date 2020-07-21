fd91a631ae95c1df40826f6d9f8fd17dc14bb6c0
HeavyManiac <30855332+HeavyManiac@users.noreply.github.com>
2020-07-21 16:47:56 -0600

    Create Tiger.java

---
+++ src/main/java/com/java/myApp/zoo/Tiger.java
@@ -0,0 +1,15 @@
+package com.java.myApp.zoo;
+
+public class Tiger extends Animal {
+    public void eat()
+    {
+        System.out.println("Tiger is eating...");
+    }
+
+    //Do not overwrrite
+    /*public void age(){
+       // this.age=45; this violates encapsulation
+    }
+
+     */
+}
