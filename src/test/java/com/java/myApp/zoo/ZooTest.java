4fcc5b331604b93a370f6ffefce2beb99a3397d3
HeavyManiac <30855332+HeavyManiac@users.noreply.github.com>
2020-07-21 16:46:14 -0600

    Create ZooTest.java

---
+++ src/test/java/com/java/myApp/zoo/ZooTest.java
@@ -0,0 +1,21 @@
+package com.java.myApp.zoo;
+
+import org.junit.Test;
+
+public class ZooTest {
+
+    @Test
+    public void shouldFeedAllAnimals(){
+        Zoo zoo = new Zoo();
+
+        Animal[] animals =
+                {
+                        new Dog(),
+                        new Gorilla(),
+                        new Lion(),
+                        new Tiger()
+                };
+        zoo.feedAnimals(animals);
+    }
+
+}
